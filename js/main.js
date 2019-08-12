b27b1cfeffa3b7ccc2d946083df1f5f24691a1f4
Joseph Tully <jtully84@gmail.com>
2019-08-12 11:14:59 -0400

    added logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = (cards[0]);
-var cardTwo = (cards[1]);
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-		alert("youve found a match!");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("you've found a match!");
 	} else {
-		alert("try again")
+		console.log("try again")
+	};
 };
+function flipCard(cardId) {
+	console.log("user flipped " + cards[cardId]);
+		cardsInPlay.push(cards[cardId]);
+		if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
+flipCard(0);
+flipCard(2);
