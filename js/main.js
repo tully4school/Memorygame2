1829e9f9c8785f642c44ec5b1cc2050d798763ba
Joseph Tully <jtully84@gmail.com>
2019-08-12 08:08:33 -0400

    added life to the js file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("user flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = (cards[0]);
+var cardTwo = (cards[1]);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("youve found a match!");
+	} else {
+		alert("try again")
+};
