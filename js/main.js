f426118ed2e13e0fa8220387c7c5a75980eccb39
Joseph Tully <jtully84@gmail.com>
2019-08-17 14:45:28 -0400

    added more JS

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("you've found a match!");
 	} else {
-		console.log("try again")
+		console.log("Sorry try again")
 	};
 };
 function flipCard(cardId) {
-	console.log("user flipped " + cards[cardId]);
-		cardsInPlay.push(cards[cardId]);
+	console.log("user flipped " + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+		console.log(cards[cardId].suit);
+		console.log(cards[cardId].cardImage);
 		if (cardsInPlay.length === 2) {
 		checkForMatch();
 	};
